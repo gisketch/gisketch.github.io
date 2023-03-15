4fc023024648c587db2d7078c891e5209683452a
gisketch <agjmnz@gmail.com>
2023-03-16 02:06:10 +0800

    minor bug fixes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -35,7 +35,7 @@ const navs = [
 
 function Nav(props) {
     const [navOpen, setNavOpen] = useState(false);
-    const [isMobile, setIsMobile] = useState(false);
+    const [isMobile, setIsMobile] = useState(true);
 
     useEffect(() => {
       const handleResize = () => {
