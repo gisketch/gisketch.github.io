f5cd9fff144a3e23c12a502e9ef3f7307f27ec6d
gisketch <agjmnz@gmail.com>
2023-03-15 14:14:08 +0800

    add: typing-text-anim and homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { useState } from 'react'
 import './App.css'
 import { motion } from "framer-motion"
+import { TypeAnimation } from 'react-type-animation'
+
+import portrait from './assets/images/me.png'
+
 
 function App() {
-
   return (
     <div className="App">
       <motion.div
@@ -54,7 +57,85 @@ function App() {
         </motion.div>
       </motion.div>
       <div className="Home">
-
+          <div className="Name">
+            <motion.h1
+              initial={
+                {
+                  opacity: 0,
+                  x: -100,
+                }
+              }
+              animate={
+                {
+                  opacity: 1,
+                  x: 0,
+                }
+              }
+              transition={
+                {
+                  duration: 0.5,
+                  delay: 1.5,
+                }
+              }>
+              Ghegi<br />
+              Jimenez
+            </motion.h1>
+            <motion.div
+              initial={
+                {
+                  opacity: 0,
+                }
+              }
+              animate={
+                {
+                  opacity: 1,
+                }
+              }
+              transition={
+                {
+                  duration: 0.5,
+                  delay: 1.75,
+                }
+              }>
+              <TypeAnimation
+              sequence={[
+                'WEB DEVELOPER', 
+                2000, 
+                'GAME DEVELOPER', 
+                2000, 
+                'ARTIST',
+                2000,
+                () => {}
+              ]}
+              wrapper="div"
+              cursor={true}
+              repeat={Infinity}
+            />
+            </motion.div>
+            
+          </div>
+          <motion.img 
+            initial={
+              {
+                opacity: 0,
+                y: 150,
+              }
+            }
+            animate={
+              {
+                opacity: 1,
+                y: 0,
+              }
+            }
+            transition={
+              {
+                duration: 0.5,
+                delay: 2,
+              }
+            }
+            className="Portrait" 
+            src={portrait} 
+            alt="Me in ASCII" />
       </div>
     </div>
   )
