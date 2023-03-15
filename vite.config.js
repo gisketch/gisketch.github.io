e702484a010b550d582071c0ca4925bc300b919c
gisketch <agjmnz@gmail.com>
2023-03-15 12:10:32 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/gisketch.github.io/"
+  base: "./gisketch.github.io/"
 })
