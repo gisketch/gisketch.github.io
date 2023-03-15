375476e6cebb0f90138e4ea77723c413c0516f61
gisketch <agjmnz@gmail.com>
2023-03-15 14:56:36 +0800

    add: responsiveness (nav)

--- src/App.jsx
+++ src/App.jsx
@@ -1,97 +1,19 @@
 import { useState } from 'react'
 import './App.css'
+import Nav from './components/Nav'
 import { motion } from "framer-motion"
 import { TypeAnimation } from 'react-type-animation'
+import { FaBars } from 'react-icons/fa'
 
 import portrait from './assets/images/me.png'
 
-const navs = [
-  {
-    name: "home",
-    link: "#",
-    color: "var(--yellow)",
-  },
-  {
-    name: "about",
-    link: "#",
-    color: "var(--green)",
-  },
-  {
-    name: "skills",
-    link: "#",
-    color: "var(--red)",
-  },
-  {
-    name: "projects",
-    link: "#",
-    color: "var(--purple)",
-  },
-  {
-    name: "contact",
-    link: "#",
-    color: "var(--blue)",
-  },
-]
 
 
 function App() {
+  
   return (
     <div className="App">
-      <motion.div
-        className="Nav"
-        initial={
-          {
-            opacity: 0,
-          }
-        }
-        animate={
-          {
-            opacity: 1,
-          }
-        }
-        transition={
-          {
-            duration: 0.75,
-          }
-        }>
-        <motion.h2>
-            GISKETCH 2023
-        </motion.h2>
-        <div>
-          {navs.map((nav, index) => (
-            <motion.a
-              key={index}
-              whileHover={
-                {
-                  color: nav.color,
-                  scale: 1.1,
-                }
-              }
-              >
-              {nav.name}
-            </motion.a>
-          ))}
-        </div>
-        <motion.div
-          className="BorderNav"
-          initial={
-            {
-              width: 0,
-            }
-          }
-          animate={
-            {
-              width: "97.5%",
-            }
-          }
-          transition={
-            {
-              duration: 0.5,
-              delay: 0.75,
-            }
-          }>
-        </motion.div>
-      </motion.div>
+      <Nav />
       <div className="Home">
           <div className="Name">
             <motion.h1
