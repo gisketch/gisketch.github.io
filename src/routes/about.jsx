d255d719196a34b05ff25a5568af08a1d19981a0
gisketch <agjmnz@gmail.com>
2023-03-15 19:57:48 +0800

    typewriter effect about revamp

--- src/routes/about.jsx
+++ src/routes/about.jsx
@@ -6,135 +6,146 @@ import {
 import {
   useState
 } from 'react';
+import { TypeAnimation } from 'react-type-animation';
+
+const Paragraph = ({ paragraphState, typing, setTyping }) => {
+
+  const renderParagraph = () => {
+    switch (paragraphState) {
+      case 1:
+        return (
+          <motion.p className={"Paragraph About-1"}>
+            {typing ? (
+              <TypeAnimation
+                sequence={[
+                  `My passion for computers started when I was just three years old, and I was allowed to use the computer at my parents' printing shop. As I grew older, I became interested in gaming and taught myself how to animate stick figures and hand-draw animations of DBZ anime fights.`,
+                  750,
+                  () => {
+                    setTyping(false);
+                  },
+                ]}
+                speed={99}
+                cursor={true}
+                className="TypeAnimation"
+              />
+            ) : (
+              <motion.span animate={{ scale: typing ? 1.1 : 1 }}>
+                My passion for <b>computers</b> started when I was just three years old,
+                and I was allowed to use the computer at my parents' printing shop.
+                As I grew older, I became interested in gaming and taught myself how
+                to <b>animate</b> stick figures and hand-draw animations of DBZ anime fights.
+              </motion.span>
+            )}
+          </motion.p>
+        );
+      case 2:
+        return (
+          <motion.p className={"Paragraph About-2"}>
+            {typing ? (
+              <TypeAnimation
+                sequence={[
+                  `In senior high school, I tried developing games by watching tutorials on YouTube, and I fell in love with programming. Despite pursuing a degree in electronics engineering in college, I spent most of my time learning programming on my own through online resources. My hard work paid off when I won first place in a Game Development competition in the whole Mindanao Region and a National Programming Competition, both of which I won on my own without any guidance.`,
+                  750,
+                  () => {
+                    setTyping(false);
+                  },
+                ]}
+                speed={99}
+                cursor={true}
+                className="TypeAnimation"
+              />
+            ) : (
+              <motion.span animate={{ scale: typing ? 1.1 : 1 }}>
+                In senior high school, I tried developing games by watching tutorials on YouTube,
+                and I fell in love with <b>programming</b>. Despite pursuing a degree in electronics
+                engineering in college, I spent most of my time learning programming on my own through
+                online resources. My hard work paid off when I won first place in a <b>Game
+                Development competition</b> in the whole Mindanao Region and a <b>National Programming Competition</b>,
+                both of which I won on my own without any guidance.
+              </motion.span>
+            )}
+          </motion.p>
+        );
+      case 3:
+        return (
+          <motion.p className={"Paragraph About-3"}>
+            {typing ? (
+              <TypeAnimation
+                sequence={[
+                  `My hard work paid off when I won first place in a Game Development competition in the whole Mindanao Region and a National Programming Competition, both of which I won on my own without any guidance.`,
+                  750,
+                  () => {
+                    setTyping(false);
+                  },
+                ]}
+                speed={99}
+                cursor={true}
+                className="TypeAnimation"
+              />
+            ) : ( 
+              <motion.span animate={{ scale: typing ? 1.1 : 1 }}>
+                My hard work paid off when I won first place in a <b>Game
+                Development competition</b> in the whole Mindanao
+                Region and a <b>National Programming Competition</b>,
+                both of which I won on my own without any guidance.
+              </motion.span>
+            )}
+          </motion.p>
+        );
+      case 4:
+        return (
+          <motion.p className={"Paragraph About-4"}>
+            {typing ? (
+              <TypeAnimation
+                sequence={[
+                  `This portfolio website is a testament to my skills and hard work, and I hope it inspires others to pursue their passions and work hard to achieve their goals. Thank you for visiting my website, and I hope you enjoy browsing through my projects.`,
+                  750,
+                  () => {
+                    setTyping(false);
+                  },
+                ]}
+                speed={99}
+                cursor={true}
+                className="TypeAnimation"
+              />
+            ) : ( 
+              <motion.span animate={{ scale: typing ? 1.1 : 1 }}>
+                This portfolio website is a testament to my <b>skills</b> and hard work,
+                and I hope it inspires others to pursue their passions and work
+                hard to achieve their goals. Thank you for visiting my website,
+                and I hope you enjoy browsing through my <b>projects</b>.
+              </motion.span>
+            )}
+          </motion.p>
+        );
+      default:
+        return null;
+    }
+  };
+
+  return <>{renderParagraph()}</>
+}
 
 function About() {
 
   const [paragraphState, setParagraphState] = useState(1);
+  const [typing, setTyping] = useState(true);
+
+
 
   return(
       <div>
         <Nav color="var(--green)"/>
         <div className="About" onClick={()=>{
-          if (paragraphState === 4) {
+          if (paragraphState === 4 && typing === false) {
             setParagraphState(1);
-          } else {
+            setTyping(true);
+          } else if ( paragraphState < 4 && typing === false){
             setParagraphState(paragraphState + 1);
+            setTyping(true);
           }
         }}>
-            <motion.p
-              className={"Paragraph About-1"}
-              initial={
-                {
-                  opacity: 0.3,
-                  y: -100,
-                  scale: 0.8,
-                }
-              }
-              animate={
-                {
-                  opacity: paragraphState === 1 ? 1 : 0.3,
-                  y: 0 + (paragraphState - 1) * -400,
-                  scale: paragraphState === 1 ? 1 : 0.8,
-                }
-              }
-              transition={
-                {
-                  duration: 0.5,
-                }
-              }
-            >
-              My passion for <b>computers</b> started when I was just three years old,
-              and I was allowed to use the computer at my parents' printing shop.
-              As I grew older, I became interested in gaming and taught myself how
-              to <b>animate</b> stick figures and hand-draw animations of DBZ anime fights.
-            </motion.p>
-            
-            <motion.p
-              className={"Paragraph About-2"}
-              initial={
-                {
-                  opacity: 0.3,
-                  y: -100,
-                  scale: 0.8,
-                }
-              }
-              animate={
-                {
-                  opacity: paragraphState === 2 ? 1 : 0.3,
-                  y: 0 + (paragraphState - 1) * -400,
-                  scale: paragraphState === 2 ? 1 : 0.8,
-                }
-              }
-              transition={
-                {
-                  duration: 0.5,
-                }
-              }
-            >
-              In senior high school, I tried developing games by watching tutorials on YouTube,
-               and I fell in love with <b>programming</b>. Despite pursuing a degree in electronics 
-               engineering in college, I spent most of my time learning programming on my own 
-               through online resources.
-            </motion.p>
-            
-            <motion.p
-              className={"Paragraph About-3"}
-              initial={
-                {
-                  opacity: 0.3,
-                  y: -100,
-                  scale: 0.8,
-                }
-              }
-              animate={
-                {
-                  opacity: paragraphState === 3 ? 1 : 0.3,
-                  y: 0 + (paragraphState - 1) * -400,
-                  scale: paragraphState === 3 ? 1 : 0.8,
-                }
-              }
-              transition={
-                {
-                  duration: 0.5,
-                }
-              }
-            >
-              My hard work paid off when I won first place in a <b>Game
-              Development competition</b> in the whole Mindanao
-              Region and a <b>National Programming Competition</b>,
-              both of which I won on my own without any guidance.
-            </motion.p>
-            
-            <motion.p
-              className={"Paragraph About-4"}
-              initial={
-                {
-                  opacity: 0.3,
-                  y: -100,
-                  scale: 0.8,
-                }
-              }
-              animate={
-                {
-                  opacity: paragraphState === 4 ? 1 : 0.3,
-                  y: 0 + (paragraphState - 1) * -400,
-                  scale: paragraphState === 4 ? 1 : 0.8,
-                }
-              }
-              transition={
-                {
-                  duration: 0.5,
-                }
-              }
-            >
-              
-            This portfolio website is a testament to my <b>skills</b> and hard work,
-            and I hope it inspires others to pursue their passions and work
-            hard to achieve their goals. Thank you for visiting my website,
-            and I hope you enjoy browsing through my <b>projects</b>.
-            </motion.p>
-            
-            {/* <p className="ScrollDown">CONTINUE</p> */}
+            <Paragraph paragraphState={paragraphState} typing={typing} setTyping={setTyping}/>
         </div>
       </div>
     )
